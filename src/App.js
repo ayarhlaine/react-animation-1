f4973cb6c268427da9f36e91402d3decc300265b
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-09-24 21:33:36 +0630

    ✨ Added Basic Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Basic from './Basic/Basic';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="App__title">React Spring Animations</h1>
+      <Basic />
     </div>
   );
 }
---
+++ src/Basic/Basic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Basic() {
+    return (
+        <div className="Basic">
+           <h2>Basic</h2>
+        </div>
+    )
+}
+
+export default Basic
